d1b031d2192c307d2439ec60249bca6ff6c58e94
pragyan154 <pragyan154@gmail.com>
2023-06-07 13:06:42 +0530

    added file of mysql connector

---
+++ dbconnect.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+# Establish a connection to the MySQL server
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root1234",
+    database="c361cohort"
+)
+
+cursor = connection.cursor()
+create_tablequery = """CREATE TABLE IF not exists table_name1( column1 varchar(255), column2 int)"""
+
+cursor.execute(create_tablequery)
+print("table created succesfully")
+
+insert_query = "insert into table_name1 (column1, column2) values(%s,%s)"
+
+data = [("value1", 22),("value2",99)]
+cursor.executemany(insert_query,data)
+
+connection.commit()
+
+selectquery = "select * from table_name1"
+cursor.execute(selectquery)
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.close()
+connection.close()
